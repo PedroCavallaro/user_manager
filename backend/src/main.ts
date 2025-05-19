1df013fe56bee11c871f8b0c8fb8c95f70469cf7
PedroCavallaro <pedrolemecavallaro@gmail.com>
2025-05-19 06:15:13 -0300

    fix: cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document)
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true
   })
 
